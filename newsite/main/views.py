e6d5c36366bfeacd1ac6952388949bd964f5e498
CymiCymi <151457887+CymiCymi@users.noreply.github.com>
2024-11-30 13:50:53 +0300

    Add files via upload

---
+++ newsite/main/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def begin(request):
+    return render(request, 'main/begin.html')
+
+def map(request):
+    return render(request, 'main/map.html')
+
+def one_all(request):
+    return render(request, 'main/one_all.html')
+
+def one_moda(request):
+    return render(request, 'main/one_moda.html')
+
+def one_country(request):
+    return render(request, 'main/one_country.html')
+
+def one_culture(request):
+    return render(request, 'main/culture.html')
+
+def two_all(request):
+    return render(request, 'main/two_all.html')
+
+def two_religion(request):
+    return render(request, 'main/two_religion.html')
+
+def two_art(request):
+    return render(request, 'main/two_art.html')
+
+def two_moda(request):
+    return render(request, 'main/two_moda.html')
+
+def two_country(request):
+    return render(request, 'main/.two_country.html')
+
+def three_all(request):
+    return render(request, 'main/three_all.html')
+
+def three_religion(request):
+    return render(request, 'main/three_religion.html')
+
+def three_art(request):
+    return render(request, 'main/three_art.html')
+
+def three_moda(request):
+    return render(request, 'main/three_moda.html')
+
+def three_country(request):
+    return render(request, 'main/three_country.html')
+
+def four_all(request):
+    return render(request, 'main/four_all.html')
+
+def four_religion(request):
+    return render(request, 'main/four_religion.html')
+
+def four_art(request):
+    return render(request, 'main/four_art.html')
+
+def four_moda(request):
+    return render(request, 'main/four_moda.html')
+
+def four_country(request):
+    return render(request, 'main/four_country.html')
+
+def five_all(request):
+    return render(request, 'main/five_all.html')
+
+def five_religion(request):
+    return render(request, 'main/five_religion.html')
+
+def five_art(request):
+    return render(request, 'main/five_art.html')
+
+def five_moda(request):
+    return render(request, 'main/five_moda.html')
+
+def five_country(request):
+    return render(request, 'main/five_country.html')
+
+def six_all(request):
+    return render(request, 'main/six_all.html')
+
+def six_religion(request):
+    return render(request, 'main/six_religion.html')
+
+def six_art(request):
+    return render(request, 'main/six_art.html')
+
+def six_moda(request):
+    return render(request, 'main/six_moda.html')
+
+def six_country(request):
+    return render(request, 'main/six_country.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+def about_us(request):
+    return render(request, 'main/abut_us.html')
+
+
